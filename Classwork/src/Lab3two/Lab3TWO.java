3ae04c27fec937c49443d4d804afc65798bff289
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-14 12:28:16 -0500

    Added new Class and signatures of to be written methods

---
+++ Classwork/src/Lab3two/Lab3TWO.java
@@ -0,0 +1,24 @@
+package Lab3two;
+
+public class Lab3TWO 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		return null;
+	}
+	
+	public static String[] mergeSort(String[] list)
+	{
+		return null;
+	}
+	
+	public static int partition(int[] list)
+	{
+		return 1;
+	}
+}
