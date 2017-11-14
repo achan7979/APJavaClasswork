99e3cbdfcc375da51ef83bc72f2958719b519aad
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-14 12:40:20 -0500

    added if statement

--- Classwork/src/LearnJavaTheHardWay/Recursion.java
+++ Classwork/src/LearnJavaTheHardWay/Recursion.java
@@ -7,17 +7,21 @@ public class Recursion
 		int[] test1 = {1,2,3,4,5,6,7,8,9,10};
 		int[] test2 = {0,4,5,6,12,15,17,23,29};
 		
-		System.out.println(binarySearch(test1,3,0,test1.length));  //2
-		System.out.println(binarySearch(test2,23,0,test1.length)); //7
+		System.out.println(binarySearch(test1,2,0,test1.length));  //1
+		System.out.println(binarySearch(test2,5,0,test1.length)); //2
 		System.out.println(binarySearch(test1,1,0,test1.length));  //0
-		System.out.println(binarySearch(test2,25,0,test1.length)); //-1
+		System.out.println(binarySearch(test2,30,0,test1.length)); //-1
 	}
 	
 	public static int binarySearch(int[] nums, int query, int first, int last)
 	{
-		if(last >= first) //if still have some array left to search
+		if(query > nums[nums.length-1] || query < nums[0])
 		{
-			int index = (first+last)/2;
+			return -1;
+		}
+		if(last > first) //if still have some array left to search
+		{
+			int index = (first+last-1)/2;
 			int guess = nums[index];
 			if(guess == query)
 			{
@@ -25,9 +29,9 @@ public class Recursion
 			}
 			if(guess > query)
 			{
-				return binarySearch(nums,query,first,index-1);
+				return binarySearch(nums,query,first,index);
 			}
-			return binarySearch(nums,query,index,last-1);
+			return binarySearch(nums,query,index+1,last);
 		}
 		return -1;
 	}
