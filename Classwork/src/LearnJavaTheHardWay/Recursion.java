bf1e1dc76b242a82e5825c260d250471ecdca8fb
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-15 12:09:27 -0500

    Fixed binarySearch method

--- Classwork/src/LearnJavaTheHardWay/Recursion.java
+++ Classwork/src/LearnJavaTheHardWay/Recursion.java
@@ -4,24 +4,26 @@ public class Recursion
 {
 	public static void main(String[] args)
 	{
-		int[] test1 = {1,2,3,4,5,6,7,8,9,10};
-		int[] test2 = {0,4,5,6,12,15,17,23,29};
+		int[] test1 = {0,1,2,3,4,5,6,7,8,9};
+		int[] test2 = {0,1,2,3,4,5,6,7,8};
 		
-		System.out.println(binarySearch(test1,2,0,test1.length));  //1
-		System.out.println(binarySearch(test2,5,0,test1.length)); //2
-		System.out.println(binarySearch(test1,1,0,test1.length));  //0
-		System.out.println(binarySearch(test2,30,0,test1.length)); //-1
+		for(int i = -1; i < test1.length+1; i++)
+		{
+			System.out.print(binarySearch(test1,i,0,test1.length)+" ");
+		}
+		System.out.println();
+		for(int i = -1; i < test2.length+1; i++)
+		{
+			System.out.print(binarySearch(test2,i,0,test2.length)+" ");
+		}
+		System.out.println();
 	}
 	
 	public static int binarySearch(int[] nums, int query, int first, int last)
 	{
-		if(query > nums[nums.length-1] || query < nums[0])
+		if(first<last) //if still have some array left to search
 		{
-			return -1;
-		}
-		if(last > first) //if still have some array left to search
-		{
-			int index = (first+last-1)/2;
+			int index = (first+last)/2;
 			int guess = nums[index];
 			if(guess == query)
 			{
