1f5f017ddfa3b376862da1f33921c2fef7152307
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-16 13:18:08 -0500

    added comments to help code the thingy mithingy

--- Classwork/src/Lab3two/Lab3TWO.java
+++ Classwork/src/Lab3two/Lab3TWO.java
@@ -4,12 +4,42 @@ public class Lab3TWO
 {
 	public static void main(String[] args) 
 	{
+		String[] test1 = {"apple","apostle","enigma"};
+		String[] test2 = {"raven","west"};
 		
+		merge(test1,test2);
+		test1[0].charAt(0);
 	}
+	/*
+	 *  String[] test1 = {"one","three,","two"};
+		String[] test2 = {"five","four","six"};
+
+		five, four, one, six, three, two
 	
+		check one and five
+		test1[0].compareTo(test2[0])
+		if (the return is negative)
+		we take test1[0]
+		else
+		we take test2[0]
+
+		merge sort
+		you break up all the arrays
+		into other arrays
+		if length>1 keep splitting
+		until no more splits are possible
+		then u merge
+		return false
+
+	 */
 	public static String[] merge(String[] list1, String[] list2)
 	{
-		return null;
+		
+		int letterAt = 0;
+		for()
+		{
+			return null;
+		}
 	}
 	
 	public static String[] mergeSort(String[] list)
