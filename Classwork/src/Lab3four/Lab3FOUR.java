aac1a74492eebfdfd0e51ab296fb77a455cfe9ac
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-27 12:25:14 -0500

    Started Lab 3.4 Added partition and swap method as helpers to create the quickSort method

---
+++ Classwork/src/Lab3four/Lab3FOUR.java
@@ -0,0 +1,56 @@
+package Lab3four;
+
+public class Lab3FOUR 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		
+	}
+	
+	/**
+	 * This method takes the first element of the array and sets it as the pivot. The
+	 * pivot is used to test every element and see whether or not they are less or greater
+	 * than the pivot. All elements that are less than the pivot are swapped and moved to 
+	 * the left of the pivot and all elements that are greater than the pivot are swapped
+	 * and moved to the right of the pivot.
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public static int partition(int[] list)
+	{
+		int pivot = list[0];
+		int i = 0;
+		for (int j = 1; j < list.length; j++)
+		{
+			if (list[j] <= pivot)
+			{
+				i++;
+				swap(list, i, j);
+			}
+		}
+		swap(list, i, 0);
+		return i;
+	}
+	
+	/**
+	 * This helper method swaps elements of the int[] at i and j by setting int[i] to a temp
+	 * so that the value isn't lost. It then sets int[i] to int[j] and setting int[j] 
+	 * to the temp
+	 * 
+	 * @param a the int[] that is passed in where elements need to be swapped
+	 * @param i	the first number that needs to be swapped with the second
+	 * @param j the second number that needs to be swapped with the first
+	 */
+	private static void swap(int[] a, int i, int j)
+	{
+	    int temp = a[i];
+	    a[i] = a[j];
+	    a[j] = temp;
+	}
+}
