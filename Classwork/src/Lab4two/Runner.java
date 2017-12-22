87b895759151e129ec004599ab7982ab4d262db8
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-12-22 12:39:20 -0500

    yes

--- Classwork/src/Lab4two/Runner.java
+++ Classwork/src/Lab4two/Runner.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Runner 
-{s
+{
 	public static void main(String[] args)
 	{
 		File CSVData = new File("Quality_Of_Life_Indicators.csv");
