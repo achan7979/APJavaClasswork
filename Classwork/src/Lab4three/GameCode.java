2b740f4aedcfb14ccdc34ceeb8ee899a70ab9068
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-01-08 13:14:55 -0500

    Finished game. Need to work on writing data onto the CSV file.

--- Classwork/src/Lab4three/GameCode.java
+++ Classwork/src/Lab4three/GameCode.java
@@ -1,10 +1,12 @@
 package Lab4three;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -25,8 +27,8 @@ public class GameCode extends Application
 		primaryStage.setTitle("Button Clicker");
 		Button btn = new Button();
 		btn.setText("Click!");
-		Text playerScore = new Text(10,0, "Score: ");
-		Text status = new Text(10,50, "Game On!");
+		Text playerScore = new Text("Score: ");
+		Text status = new Text("Game On!");
 
 		btn.setOnAction(new EventHandler<ActionEvent>() 
 		{
@@ -40,25 +42,30 @@ public class GameCode extends Application
 			}
 		});
 	
-		timer = System.nanoTime() + 1000000000L;
+		timer = System.nanoTime() + 10000000000L;
 		new AnimationTimer()
 		{
 			public void handle(long now)
 			{
 				if(now > timer)
 				{
-					timer = now + 1000000000L;
+					timer = now + 10000000000L;
 					gameOn = !gameOn;
 				}
 				if(!gameOn)
 				{
 					btn.setDisable(true);
+					status.setText("Game Over!");
 				}
 				
-				playerScore.setText("Score: " + Integer.toString(score));
+				playerScore.setText("Score: " + Integer.toString(score) + "       ");
 			}
 		}.start();
 		StackPane root = new StackPane();
+		HBox hbox = new HBox();
+		root.getChildren().add(hbox);
+		hbox.getChildren().addAll(playerScore);
+		hbox.getChildren().addAll(status);
 		root.getChildren().add(btn);
 		primaryStage.setScene(new Scene(root, 300, 250));
 		primaryStage.show();
